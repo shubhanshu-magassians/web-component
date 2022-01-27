5f0e77ee91c15fc745eebf6ccdcfce824679b41d
shubhanshuyadav <shubhanshuemailid@gmail.com>
2022-01-27 20:26:44 +0530

    vite added

--- web-component-card/src/main.js
+++ web-component-card/src/main.js
@@ -1,7 +1,5 @@
-import { createApp } from 'vue'
 import { defineCustomElement } from 'vue'
 
-import App from './App.vue'
 import Card from  './components/Card.vue'
 
 const wrappedElement = defineCustomElement(Card);
